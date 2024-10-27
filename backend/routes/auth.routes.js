a75b300bb53962f4bbabfa0c713fd9d650013362
taiga-shiokawa <taiga.hr12@gmail.com>
2024-10-27 22:53:24 +0900

    パスワード再リセット修正

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/reset-password", forgotPassword);
+router.post("/forgot-password", forgotPassword);
 
 router.get("/me", protectRoute, getCurrentUser);
 
